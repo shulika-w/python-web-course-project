9a4954bf5e233927e2775db32a4190be972ecbcf
shulika-w <shulika.w@gmail.com>
2024-10-13 04:10:48 -0700

    change role access and users CRUD

--- app/src/routes/users.py
+++ app/src/routes/users.py
@@ -3,24 +3,34 @@ Module of users' routes
 """
 
 
-import cloudinary
-import cloudinary.uploader
+from dataclasses import asdict
+from typing import Annotated
+
 from fastapi import APIRouter, HTTPException, Depends, UploadFile, File, status
 from redis.asyncio.client import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.src.database.connect_db import get_session, get_redis_db1
-from app.src.database.models import User
+from app.src.database.models import User, Role
 from app.src.repository import users as repository_users
 from app.src.services.auth import auth_service
-from app.src.conf.config import settings
-from app.src.schemas.users import UserDb
+from app.src.services.roles import RoleAccess
+from app.src.schemas.users import UserDb, UserUpdateModel, UserSetRoleModel, UserUpdateForm
 
 
 router = APIRouter(prefix="/users", tags=["users"])
 
+allowed_operations_read_update = RoleAccess(
+    [Role.administrator, Role.moderator, Role.user]
+)
+allowed_operations_activate_inactivate_set_role = RoleAccess([Role.administrator])
 
-@router.get("/me", response_model=UserDb)
+
+@router.get(
+    "/me",
+    response_model=UserDb,
+    dependencies=[Depends(allowed_operations_read_update)],
+)
 async def read_me(user: User = Depends(auth_service.get_current_user)):
     """
     Handles a GET-operation to '/me' users subroute and gets the current user.
@@ -33,19 +43,50 @@ async def read_me(user: User = Depends(auth_service.get_current_user)):
     return user
 
 
-@router.patch("/avatar", response_model=UserDb)
-async def update_avatar(
-    file: UploadFile = File(),
+@router.get(
+    "/{username}",
+    response_model=UserDb,
+    dependencies=[Depends(allowed_operations_read_update)],
+)
+async def read_user(username: str, session: AsyncSession = Depends(get_session)):
+    """
+    Handles a GET-operation to '/{username}' users subroute and gets the user's profile with specified username.
+
+    :param username: The username of the user to read.
+    :type username: str
+    :param session: The database session.
+    :type session: AsyncSession
+    :return: The user's profile with specified username.
+    :rtype: User
+    """
+    user = await repository_users.get_user_by_username(username, session)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
+    return user
+
+
+@router.put(
+    "/me",
+    response_model=UserDb,
+    dependencies=[Depends(allowed_operations_read_update)],
+)
+async def update_me(
+    data: UserUpdateForm = Depends(),
+    file: Annotated[UploadFile, File()] = None,
     user: User = Depends(auth_service.get_current_user),
     session: AsyncSession = Depends(get_session),
     cache: Redis = Depends(get_redis_db1),
 ):
     """
-    Handles a PATCH-operation to '/avatar' users subroute and updates an user's avatar.
+    Handles a PUT-operation to '/me' users subroute and updates the current user's profile.
 
+    :param data: The data for the user to update.
+    :type data: UserUpdateModel
     :param file: The uploaded file to update avatar from.
     :type file: UploadFile
-    :param user: The user to update avatar.
+    :param user: The current user.
     :type user: User
     :param session: The database session.
     :type session: AsyncSession
@@ -54,26 +95,104 @@ async def update_avatar(
     :return: The updated user.
     :rtype: User
     """
-    cloudinary.config(
-        cloud_name=settings.cloudinary_cloud_name,
-        api_key=settings.cloudinary_api_key,
-        api_secret=settings.cloudinary_api_secret,
-        secure=True,
-    )
-    api_name = settings.api_name.replace(" ", "_")
     try:
-        r = cloudinary.uploader.upload(
-            file.file,
-            public_id=f"{api_name}/{user.username}",
-            overwrite=True,
-        )
-        src_url = cloudinary.CloudinaryImage(f"{api_name}/{user.username}").build_url(
-            width=250, height=250, crop="fill", version=r.get("version")
-        )
+        data = UserUpdateModel(**asdict(data))
     except Exception as error_message:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Upload image error: {str(error_message)}",
+            detail=str(error_message),
         )
-    user = await repository_users.update_avatar(user.email, src_url, session, cache)
-    return user
+    return await repository_users.update_user(user.email, data, file, session, cache)
+
+
+@router.patch(
+    "/{username}/set_role",
+    response_model=UserDb,
+    dependencies=[Depends(allowed_operations_activate_inactivate_set_role)],
+)
+async def set_role_for_user(
+    username: str,
+    body: UserSetRoleModel,
+    session: AsyncSession = Depends(get_session),
+    cache: Redis = Depends(get_redis_db1),
+):
+    """
+    Handles a PATCH-operation to '/{username}/set_role' users subroute and sets an user's role.
+
+    :param username: The username of the user to set role.
+    :type username: str
+    :param body: The request body with data for an user's role to set.
+    :type body: UserSetRoleModel
+    :param session: The database session.
+    :type session: AsyncSession
+    :param cache: The Redis client.
+    :type cache: Redis
+    :return: The user for whom the role is set.
+    :rtype: User
+    """
+    user = await repository_users.get_user_by_username(username, session)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
+    return await repository_users.set_role_for_user(username, body.role, session, cache)
+
+
+@router.patch(
+    "/{username}",
+    response_model=UserDb,
+    dependencies=[Depends(allowed_operations_activate_inactivate_set_role)],
+)
+async def activate_user(
+    username: str,
+    session: AsyncSession = Depends(get_session),
+    cache: Redis = Depends(get_redis_db1),
+):
+    """
+    Handles a PATCH-operation to '/{username}' users subroute and activates the user with specified username.
+
+    :param username: The username of the user to activate.
+    :type username: str
+    :param session: The database session.
+    :type session: AsyncSession
+    :param cache: The Redis client.
+    :type cache: Redis
+    :return: The activated user.
+    :rtype: User
+    """
+    user = await repository_users.get_user_by_username(username, session)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
+    return await repository_users.activate_user(username, session, cache)
+
+
+@router.delete(
+    "/{username}",
+    response_model=UserDb,
+    dependencies=[Depends(allowed_operations_activate_inactivate_set_role)],
+)
+async def inactivate_user(
+    username: str,
+    session: AsyncSession = Depends(get_session),
+    cache: Redis = Depends(get_redis_db1),
+):
+    """
+
+    Handles a DELETE-operation to '/{username}' users subroute and inactivates the user with specified username.
+    :param username: The username of the user to inactivate.
+    :type username: str
+    :param session: The database session.
+    :type session: AsyncSession
+    :param cache: The Redis client.
+    :type cache: Redis
+    :return: The inactivated user.
+    :rtype: User
+    """
+    user = await repository_users.get_user_by_username(username, session)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
+    return await repository_users.inactivate_user(username, session, cache)
--- app/src/schemas/users.py
+++ app/src/schemas/users.py
@@ -2,17 +2,22 @@
 Module of users' schemas
 """
 
-
+from dataclasses import dataclass
 from datetime import datetime, date
+from pydantic import (
+    BaseModel,
+    Field,
+    EmailStr,
+    HttpUrl,
+    UUID4,
+    ConfigDict,
+)
 
-from pydantic import BaseModel, Field, EmailStr, HttpUrl, UUID4, ConfigDict, field_validator, conint, constr
+from typing import Annotated
 
+from fastapi import Form
 
-def date_validator(birthday):
-    if len(birthday) == 0:
-        return None
-    birthday = datetime.strptime(birthday, "%Y-%m-%d").date()
-    return birthday
+from app.src.database.models import Role
 
 
 class UserModel(BaseModel):
@@ -22,11 +27,33 @@ class UserModel(BaseModel):
     first_name: str = Field(max_length=254)
     last_name: str = Field(max_length=254)
     phone: str = Field(max_length=38)
-    birthday: constr(min_length=10, max_length=10)
+    birthday: date | None
 
-    @field_validator('birthday')
-    def validate_birthday(cls, value):
-        return value
+
+@dataclass
+class UserCreateForm:
+    username: str = Form(...)
+    email: str = Form(...)
+    password: str = Form(...)
+    first_name: Annotated[str | None, Form(...)] = None
+    last_name: Annotated[str | None, Form(...)] = None
+    phone: Annotated[str | None, Form(...)] = None
+    birthday: Annotated[str | None, Form(...)] = None
+
+
+class UserUpdateModel(BaseModel):
+    first_name: Annotated[str | None, Field(max_length=254)]
+    last_name: Annotated[str | None, Field(max_length=254)]
+    phone: Annotated[str | None, Field(max_length=38)]
+    birthday: date | None
+
+
+@dataclass
+class UserUpdateForm:
+    first_name: Annotated[str | None, Form(...)] = None
+    last_name: Annotated[str | None, Form(...)] = None
+    phone: Annotated[str | None, Form(...)] = None
+    birthday: Annotated[str | None, Form(...)] = None
 
 
 class UserRequestEmail(BaseModel):
@@ -37,20 +64,24 @@ class UserPasswordSetModel(BaseModel):
     password: str = Field(min_length=8, max_length=72)
 
 
+class UserSetRoleModel(BaseModel):
+    role: Role
+
+
 class UserDb(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: UUID4 | int
-    username: str
+    username: str = Field(min_length=2, max_length=254)
     email: EmailStr
-    first_name: str = Field(max_length=254)
-    last_name: str = Field(max_length=254)
-    phone: str = Field(max_length=38)
+    first_name: Annotated[str | None, Field(max_length=254)]
+    last_name: Annotated[str | None, Field(max_length=254)]
+    phone: Annotated[str | None, Field(max_length=38)]
     birthday: date | None
     created_at: datetime
     updated_at: datetime
     avatar: HttpUrl
-    role: str
+    role: Role
     is_active: bool
 
 
